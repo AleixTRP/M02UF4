ee1b919fbebb4b3d821223718ca5fa215e23598f
AleixTRP <aleix.ramirez@enti.cat>
2023-02-03 11:22:02 +0100

    Hola

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/bin/node
+
+
+const http = require('http');
+
+http.createServer(function(request,result){
+	console.log("Alguien se conecta");
+	result.write('ola k ase');
+	result.end();
+}).listen(8080);
