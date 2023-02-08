48d31cfb01f85bf04dcfc8ac2dfb8877b71f7f38
AleixTRP <aleix.ramirez@enti.cat>
2023-02-08 11:05:08 +0100

    Nuevo index.js

--- index.js
+++ index.js
@@ -9,35 +9,49 @@ const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'abascal';
-
 let db;
+let collection;
 
-async function db_connect() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  db = client.db(dbName);
-  //                                                                                                                                  const collection = db.collection('documents');
-
-  // the following code examples can be pasted here...
-
-  return 'Conectados a la base de MongoDB.';
+async function dbConnect() {
+await client.connect();
+	console.log('Connected successfully to server');
+		db = client.db(dbName);
+return 'Connected to MongoDB database';
 }
-db_connect()
-  .then(info => console.log(info))
-  .catch(msg => console.error(msg));  
 
-let http_server = http.createServer(function(request, response){
-	let collection = db.collection('characters');
+dbConnect()
+.then(console.log)
+.catch(console.error);
 
-	collection.find({}).toArray().then(query => console.log(query));
+http.createServer(function(request, response) {
+console.log("Alguien se conecta");
+
+if (request.url == "/favicon.ico") {
+return;
 	
-	console.log(collection);
-	console.log("Alguien se conecta");
-	response.write('ola k ase');
-	response.end();
-})
+	}
+		if (request.url == "/Characters") {
+	
+	collection = db.collection('Characters');
+															
+collection.find({}).toArray().then(characters => {
 
-http_server.listen(6969);
+console.log(characters);
 
+let charactersName = [];
 
+for (let i = 0; i < characters.length; i++)
+{
+charactersName.push(characters[i].name);
+}
+
+response.write(JSON.stringify(charactersName));
+response.end();
+});
+}
+else {
+response.write("Página principal");
+response.end();
+}
+
+}).listen(6969);
